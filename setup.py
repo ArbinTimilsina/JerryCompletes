b5245911be86fe7686f15ed724532ca9b0fe0e12
Arbin Timilsina <arbin.timilsina@gmail.com>
2020-02-27 11:25:47 -0500

    Fix transformers version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=['jerry_completes'],
     include_package_data=True,
     install_requires=[
-        'torch', 'transformers', 'nltk', 'tqdm', 'flask', 'ftfy'
+        'torch', 'transformers==2.5.0', 'nltk', 'tqdm', 'flask', 'ftfy'
     ],
     zip_safe=False,
     classifiers=[
